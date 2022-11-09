a5e35aa2b4e12bcad3d5fd060788ae9421c00f05
Justo <ju2484s@gmail.com>
2022-11-09 16:56:49 -0500

    mov song

--- codigo_luna.py
+++ codigo_luna.py
@@ -7,15 +7,21 @@ pg.mixer.music.load(f"./audios/{canciones[0]}.mp3")
 Partitura=""
 nota=""
 play_song=False
+lista_notas=[]
+centecimas=0
+def music_time():
+    tiempo=pg.time.get_ticks()/100
+    return tiempo
 #variables#
 size=(800,600)
 screen=pg.display.set_mode(size)
 clock=pg.time.Clock()
+
 #Funcion de eventos# 
 time_Music=pg.USEREVENT +1
 pg.time.set_timer(time_Music,100)
 def events(): 
-    global Partitura , nota , play_song 
+    global Partitura , nota , play_song , centecimas , lista_notas
     for event in pg.event.get():
         if event.type==pg.QUIT:
             pg.quit()
@@ -23,12 +29,33 @@ def events():
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_SPACE:
                 pg.mixer.music.play()
+                centecimas=music_time()
                 Partitura=open(f"data_{canciones[0]}.txt", "r")
                 play_song=True 
+                nota=Partitura.readline() 
         if play_song:
             if event.type==time_Music:
-                nota=Partitura.readline()
-                print(nota)
+                numero=music_time()-centecimas
+                
+                dibujo_nota= nota.split(';')
+                if int(dibujo_nota[0])-20== int(numero):
+
+                    if dibujo_nota[1] =="0\n":
+                        lista_notas.append([100,0])
+                       
+                    if dibujo_nota[1] =="1\n":
+                       lista_notas.append([300,0]) 
+                    
+                    if dibujo_nota[1] =="2\n":
+                        lista_notas.append([500,0])
+                        
+                    if dibujo_nota[1] =="3\n":
+                        lista_notas.append([700,0])
+                    
+                    nota=Partitura.readline()    
+                print(lista_notas)
+                print(int(dibujo_nota[0]))
+
 
 
 while True:
@@ -36,8 +63,8 @@ while True:
 
     screen.fill("White")
     #zona de dibujo
-    print(nota.split(';'))
-    print(len(nota.split(';')))
+    #print(nota.split(';'))
+    #print(len(nota.split(';')))
     dibujo_nota= nota.split(';')
     if len(dibujo_nota)==2:
         if dibujo_nota[1] =="0\n":
@@ -48,6 +75,11 @@ while True:
             pg.draw.circle(screen,'Red',(500,500),50)
         if dibujo_nota[1] =="3\n":
             pg.draw.circle(screen,'Red',(700,500),50)
-
+    if len (lista_notas)>0:
+        for i in range(len(lista_notas)):
+            pg.draw.circle(screen,"Blue",lista_notas[i],50)
+            lista_notas[i][1]+=5
+            print(i)
+    music_time()
     pg.display.update()
     clock.tick(60)
