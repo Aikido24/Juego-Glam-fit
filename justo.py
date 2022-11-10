a62442a173497286368ccb7709af55fa65715e07
Aikido 24 <ju2484s@gmail.com>
2022-11-10 18:41:57 -0500

    actual

--- justo.py
+++ justo.py
@@ -12,37 +12,43 @@ def my_time():
     global current_time
     current_time= int(pg.time.get_ticks()/100)
 #music
-song ='smooth'
+song ='I’m-So-Sorry'
 pg.mixer.music.load(f"./audios/{song}.mp3") 
 data = open(f"data_{song}.txt", "a")
 button1 =False
 button2 =False
 button3 =False
+button4 =False
 #Funcion de eventos#
 def events():
-    global button1 , button2 , button3
+    global button1 , button2 , button3 , button4
     for event in pg.event.get():
         if event.type==pg.QUIT:
             pg.quit()
             exit()
         
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
+            if event.key == pg.K_q:
                 button1 =True
                 data.write(f'{current_time};0\n')
-            if event.key == pg.K_DOWN:
+            if event.key == pg.K_w:
                 button2 =True
                 data.write(f'{current_time};1\n')
-            if event.key == pg.K_RIGHT:
+            if event.key == pg.K_e:
                 button3 =True
                 data.write(f'{current_time};2\n')
+            if event.key == pg.K_r:
+                button4 =True
+                data.write(f'{current_time};3\n')
         if event.type == pg.KEYUP:
-            if event.key == pg.K_LEFT:
+            if event.key == pg.K_q:
                 button1 =False
-            if event.key == pg.K_DOWN:
+            if event.key == pg.K_w:
                 button2 =False
-            if event.key == pg.K_RIGHT:
+            if event.key == pg.K_e:
                 button3 =False
+            if event.key == pg.K_r:
+                button4 =False
 pg.mixer.music.play()
 while True:
     events()
@@ -51,13 +57,15 @@ while True:
     pg.draw.circle(screen,'Red',(100,500),50)
     pg.draw.circle(screen,'Red',(300,500),50)
     pg.draw.circle(screen,'Red',(500,500),50)
-
+    pg.draw.circle(screen,'Red',(700,500),50)
     if button1:
         pg.draw.circle(screen,'#C71EE8',(100,500),50)
     if button2:
         pg.draw.circle(screen,'#F28C1F',(300,500),50)
     if button3:
         pg.draw.circle(screen,'#561FF2',(500,500),50)
+    if button4:
+        pg.draw.circle(screen,'#561FF2',(700,500),50)
     pg.display.update()
 
     my_time()
